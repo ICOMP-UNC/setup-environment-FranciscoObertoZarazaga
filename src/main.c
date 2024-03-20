dfdc2119184d2b702049bb67883ada34ed6db9a6
Francisco Oberto <franobertozarazaga@gmail.com>
2024-03-19 23:35:40 -0300

    [step1] Init Config

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
-#include <greetings.h>
+#include "greetings.h"
+#include "resta.h"
+#include "sum.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
-    printf("Hello World");
+/**
+ * @brief main function for testing configuration.
+ * @param argc: number of arguments
+ * @param argv: array of arguments
+ * @return 1 if succes, 0 if failure
+ * @see greetings sum resta
+ * */
+int main(int argc, char* argv[])
+{
     greetings();
+    printf("suma: %i, resta: %i \n", sum(10, 5), resta(10, 5));
     return 0;
 }
